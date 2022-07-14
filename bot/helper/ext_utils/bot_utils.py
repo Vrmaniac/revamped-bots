4781147db1a9bb3bfe8e93e26e227ec70246db27
Vrmaniac <76871504+Vrmaniac@users.noreply.github.com>
2022-07-14 21:07:32 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -125,12 +125,9 @@ def get_progress_bar_string(status):
     p = 0 if total == 0 else round(completed * 100 / total)
     p = min(max(p, 0), 100)
     cFull = p // 8
-    cPart = p % 8 - 1
-    p_str = '◉' * cFull
-    if cPart >= 0:
-        p_str += PROGRESS_INCOMPLETE[cPart]
-    p_str += '○' * (PROGRESS_MAX_SIZE - cFull)
-    p_str = f"⠧{p_str}⠹"
+    p_str = '▰' * cFull
+    p_str += '▱' * (12 - cFull)
+    p_str = f"[{p_str}]"
     return p_str
 
 def get_readable_message():
@@ -360,7 +357,7 @@ CPU: {cpu}% | RAM: {mem}%
 Disk: {total} | Free: {free}
 Used: {used} [{disk}%]
 
-Made with ❤️ by Dawn
+Mr. X Mirror/Leech Zone™️
 """
     return stats
 dispatcher.add_handler(
